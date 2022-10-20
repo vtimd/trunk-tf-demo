3ee6b54df0c93a472d110c4f7ab62de066ff2f10
Tim Davis <tim.davis.instinct@gmail.com>
2022-10-20 10:11:05 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+}
+
+variable "size" {
+  description = "The Azure Virtual Machine size that is desired."
+}
+
+variable "username" {
+  description = "The Azure Virtual Machine size that is desired."
+}
+
+variable "password" {
+  description = "The Azure Virtual Machine size that is desired."
+}
